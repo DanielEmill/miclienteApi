752f178cbe5598e42b453ec2134c1f73948fe18a
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-10-19 11:56:27 -0400

    Modelos agregado

---
+++ Models/ClientesDetalle.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+public class ClientesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int ClienteId { get; set; }
+    public int TipoId { get; set; }
+    public string Telefono { get; set; }
+}
