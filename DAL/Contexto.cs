8e229b81ab24c22b76ab6967619dc4b11de2698e
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-10-19 12:09:21 -0400

    DAL UPLOAD

---
+++ DAL/Contexto.cs
@@ -0,0 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+public class Contexto: DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options): base(options) { }
+    public DbSet<Clientes> Clientes { get; set; }
+}
